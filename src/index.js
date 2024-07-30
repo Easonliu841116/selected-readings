884d3714cafa256830a01f65adcb8dd003482ffd
Anne Lin <annelin27@gmail.com>
2024-07-30 19:15:10 +0800

    index style

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import Splide from '@splidejs/splide';
 const bannerSplide = document.querySelector(".slider-banner");
 const newsSplide = document.querySelector(".splide-news");
 const guideSplide = document.querySelector(".splide-guide");
+const registrationSplide = document.querySelector(".splide-registration");
+const registrationArea = document.querySelector(".registration-area");
 
 const banner = new Splide(".splide-banner", {
     type: "loop",
     //drag: false,
-    //focus: "center",
+    focus: 1,
     //arrows: false,
     //pagination: false,
     //perPage: 4,
@@ -19,19 +21,10 @@ const banner = new Splide(".splide-banner", {
     //gap: "12px",
     breakpoints: {
         //1297: {
-        //    perPage: 4,
         //},
         //1023: {
-        //perPage: 3,
-        //fixedWidth: "205px",
-        //fixedHeight: "205px",
-        //gap: "10px",
         //},
         767: {
-            //perPage: 2,
-            //fixedWidth: "119.56px",
-            //fixedHeight: "119.56px",
-            //gap: "10px",
             arrows: false,
         },
     },
@@ -39,61 +32,65 @@ const banner = new Splide(".splide-banner", {
 
 const news = new Splide(".splide-news", {
     type: "loop",
-    //drag: false,
-    //focus: "center",
-    //arrows: false,
     pagination: false,
     perPage: 4,
-    fixedWidth: "290px",
-    fixedHeight: "336px",
-    gap: "30px",
-    //breakpoints: {
-    //1297: {
-    //    perPage: 4,
-    //},
-    1023: {
-        destroy: true,
+    fixedWidth: "300px",
+    fixedHeight: "346px",
+    gap: "20px",
+    breakpoints: {
+        1297: {
+            perPage: 3,
+
+        },
+        1023: {
+            destroy: true,
+        },
     },
-    //767: {
-    //perPage: 2,
-    //fixedWidth: "119.56px",
-    //fixedHeight: "119.56px",
-    //gap: "10px",
-    //},
-    //},
 });
 
 const guide = new Splide(".splide-guide", {
     type: "loop",
-    //drag: false,
-    //focus: "center",
-    //arrows: false,
     pagination: false,
     perPage: 6,
-    fixedWidth: "180px",
-    fixedHeight: "346px",
-    gap: "30px",
-    //breakpoints: {
-    //1297: {
-    //perPage: 4,
-    //},
-    //1023: {
-    //perPage: 3,
-    //fixedWidth: "205px",
-    //fixedHeight: "205px",
-    //gap: "10px",
-    //},
-    //767: {
-    //perPage: 2,
-    //fixedWidth: "119.56px",
-    //fixedHeight: "119.56px",
-    //gap: "10px",
-    //},
-    //},
+    fixedWidth: "190px",
+    fixedHeight: "356px",
+    gap: "20px",
+    breakpoints: {
+        1297: {
+            perPage: 4,
+        },
+        1023: {
+            arrows: false,
+            perPage: 2,
+            fixedWidth: "165px",
+            fixedHeight: "316px",
+            gap: "15px",
+        },
+    },
 });
 
-//document.addEventListener('DOMContentLoaded', function () {
+const registration = new Splide(".splide-registration", {
+    type: "slide",
+    drag: false,
+    autoWidth: true,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+        767: {
+            drag: 'free',
+        },
+    },
+});
+
 banner.mount();
 news.mount();
 guide.mount();
-//});
+registration.mount();
+
+registrationArea.querySelector('.next').addEventListener('click', () => {
+    registration.go('+${1}');
+});
+
+registration.on('click', (e) => {
+    registration.go(e.index);
+});
